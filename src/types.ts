a4b7c75965740b57e55645e4bc17e09d97c7c12e
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2021-03-02 00:32:58 +0900

    chore: formating

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 import type { Router } from "./deps.ts";
 
-type Get = Router['get'];
+type Get = Router["get"];
 type Middeware = Parameters<Get>[1];
 export type Context = Parameters<Middeware>[0];
