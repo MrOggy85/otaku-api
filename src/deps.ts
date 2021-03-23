0938d43fd372b5c0246c7c0ab292c74bfdba58e6
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2021-03-23 09:45:46 +0900

    chore: rm unused deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,10 +1,5 @@
 export { config } from "https://deno.land/x/dotenv/mod.ts";
-export {
-  Application,
-  HttpError,
-  isHttpError,
-  Router,
-} from "https://deno.land/x/oak/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak/mod.ts";
 export { DB } from "https://deno.land/x/sqlite/mod.ts";
 export { v4 as uuid } from "https://deno.land/std/uuid/mod.ts";
 export {
